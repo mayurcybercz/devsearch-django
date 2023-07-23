2574ee8422713c57e042b2d9d1dab1b578444333
Mayur <rahangdalemayur@gmail.com>
2023-07-23 19:31:25 -0400

    create and delete signals

--- users/models.py
+++ users/models.py
@@ -23,7 +23,7 @@ class Profile(models.Model):
 
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 class Skill(models.Model):
     owner=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True)
