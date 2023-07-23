1ca51092ea7c25a44b401f1e96da4e257b76edc0
Mayur <rahangdalemayur@gmail.com>
2023-07-23 04:17:12 -0400

    profiles and user-profile template

--- users/views.py
+++ users/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Profile
 
 def profiles(request):
-    context={}
+    profiles=Profile.objects.all()
+    context={'profiles':profiles}
     return render(request,'users/profiles.html',context)
+
+def userProfile(request,pk):
+    profile=Profile.objects.get(id=pk)
+    topSkills=profile.skill_set.exclude(description__exact="")
+    otherSkills=profile.skill_set.filter(description="")
+    context={'profile':profile,'topSkills':topSkills,'otherSkills':otherSkills}
+    return render(request,'users/user-profile.html',context)
