dce7ac9f1427be29c6d7a5bc5eef931498fd32b5
Mayur <rahangdalemayur@gmail.com>
2023-07-25 05:04:53 -0400

    user-edit-form

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -1,4 +1,29 @@
 // Invoke Functions Call on Document Loaded
+// document.addEventListener('DOMContentLoaded', function () {
+//   hljs.highlightAll();
+// });
+
+
+// let alertWrapper = document.querySelector('.alert')
+// let alertClose = document.querySelector('.alert__close')
+
+// if (alertWrapper) {
+//   alertClose.addEventListener('click', () =>
+//     alertWrapper.style.display = 'none'
+//   )
+// }
+
 document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
+
+  // Wrap the event listener setup inside the DOMContentLoaded event handler
+  let alertWrappers = document.querySelectorAll('.alert');
+
+  // Use event delegation to handle the close button click event
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('alert__close')) {
+      event.target.closest('.alert').style.display = 'none';
+    }
+  });
 });
+
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import Profile
-from .forms import customUserCreationForm
+from .forms import CustomUserCreationForm,ProfileForm
 
 def loginUser(request):
     page='login'
@@ -30,24 +31,24 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    messages.success(request,'User successfully logged out!')
+    messages.info(request,'User successfully logged out!')
     return(redirect('login'))
 
 def registerUser(request):
     page='register'
-    form=customUserCreationForm()
+    form=CustomUserCreationForm()
     if request.method=="POST":
-        form=customUserCreationForm(request.POST)
+        form=CustomUserCreationForm(request.POST)
         print("inpost")
         if form.is_valid():
             user=form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            print("useradded")
+
             messages.success(request,"User account created!")
 
             login(request,user)
-            return redirect('profiles')
+            return redirect('edit-account')
         else:
             messages.error(request,"Error occurred during registration")
     context={'page':page,'form':form}
@@ -64,3 +65,25 @@ def userProfile(request,pk):
     otherSkills=profile.skill_set.filter(description="")
     context={'profile':profile,'topSkills':topSkills,'otherSkills':otherSkills}
     return render(request,'users/user-profile.html',context)
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile=request.user.profile
+    skills=profile.skill_set.all()
+    projects=profile.project_set.all()
+    context={'profile':profile,'skills':skills,'projects':projects}
+    return render(request,'users/account.html',context)
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile=request.user.profile
+    form=ProfileForm(instance=profile)
+
+    if request.method=="POST":
+        form=ProfileForm(request.POST,request.FILES,instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+    context={'form':form}
+    return render(request,'users/profile_form.html',context)
