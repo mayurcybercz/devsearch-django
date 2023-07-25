299dfe8abdbaa366197e13b433bbf58ffa5fbd32
Mayur <rahangdalemayur@gmail.com>
2023-07-25 06:11:28 -0400

    user actions CRUD for skills/project/account

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import Profile
-from .forms import CustomUserCreationForm,ProfileForm
+from .forms import CustomUserCreationForm,ProfileForm,SkillForm
 
 def loginUser(request):
     page='login'
@@ -87,3 +87,47 @@ def editAccount(request):
             return redirect('account')
     context={'form':form}
     return render(request,'users/profile_form.html',context)
+
+@login_required(login_url='login')
+def createSkill(request):
+    profile=request.user.profile
+    form=SkillForm()
+
+    if request.method == 'POST':
+        form=SkillForm(request.POST)
+        if form.is_valid():
+            skill=form.save(commit=False)
+            skill.owner=profile
+            skill.save()
+            messages.success(request,"Skill added successfully!")
+            return redirect('account')
+
+    context={'form':form}
+    return render(request,'users/skill_form.html',context)
+
+@login_required(login_url='login') #only accessible if loggged-in
+def updateSkill(request,pk):
+    profile=request.user.profile
+    skill=profile.skill_set.get(id=pk) #only this user can update
+    form=SkillForm(instance=skill) #pass in instance to prefill with instance data
+
+    if request.method == 'POST':
+        form=SkillForm(request.POST,instance=skill)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Skill updated successfully!")
+            return redirect('account')
+
+    context={'form':form}
+    return render(request,'users/skill_form.html',context)
+
+def deleteSkill(request,pk):
+    profile=request.user.profile
+    skill=profile.skill_set.get(id=pk)
+
+    if request.method=='POST':
+        skill.delete()
+        messages.success(request,"Skill deleted successfully!")
+        return redirect('account')
+    context={'object':skill}
+    return render(request,'delete_template.html',context)
