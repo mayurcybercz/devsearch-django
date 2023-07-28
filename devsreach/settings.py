a860e992f669a3cdad8f842937b82c3e78b69e06
Mayur <rahangdalemayur@gmail.com>
2023-07-28 06:41:52 -0400

    SMTP mailing

--- devsreach/settings.py
+++ devsreach/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from keys import smtp_host_password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+#email backend settings
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='devsearchapp.email@gmail.com'
+EMAIL_HOST_PASSWORD= smtp_host_password
 
 
 # Static files (CSS, JavaScript, Images)
